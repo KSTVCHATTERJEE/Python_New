b93d3dbe136741ccae58de1573a8f8119ad179cf
Kaustav Chatterjee <31096402+KSTVCHATTERJEE@users.noreply.github.com>
2018-02-06 12:46:15 +0530

    6th February 2018

--- NumericalPython.py
+++ NumericalPython.py
@@ -31,7 +31,6 @@ ind=[1,2,-3]
 y=np.take(a,ind)
 print(y)
 
-#%% mask command
 mask=np.array([0,1,1,0,0,1,0,0],dtype=bool)
 y=a[mask]
 print(y)
@@ -57,9 +56,10 @@ a[mask,2]
 
 a=np.array([[1,2,3],[4,5,6]],float)
 a
+np.sum(a) #Total Sum
+a.sum() #Total Sum
 
-a.sum()
-
+sum(a)#Columnwise Sum 
 a.sum(axis=0) #Columnwise Sum
 
 a.sum(axis=1) #Rowwise Sum
@@ -70,10 +70,14 @@ a.prod(axis=0)
 
 a.prod(axis=1)
 
+
 a=np.array([2.,3.,0.,1.])
 a.min(axis=0)
 np.amin(a,axis=0)
 a.argmin(axis=0)
+
+np.amax(a,axis=0)
+np.amax(a,axis=None)
 
 a=np.array([2.,1.,0.,3.])
 a.max(axis=0)
@@ -81,7 +85,7 @@ a.max(axis=0)
 a.argmax(axis=0)
 
 a.mean(axis=0)
-
+np.average(a, weights=[1,2], axis=0)
 a.std(axis=0)
 
 a.var(axis=0)
@@ -120,3 +124,35 @@ b=np.array(((1,2,5,4),(1,3,4,5)))
 a==b
 
 np.equal(a,b)
+
+#using timeit func to check execution time
+%timeit sum(a)
+%timeit a.sum()
+%timeit np.sum(a)
+
+a.flatten()
+type(a.flatten())
+
+b=a.ravel()
+
+a.resize(3,2)
+a
+
+id(a)
+id(b)
+
+a.swapaxes(0,1)
+a
+
+p=np.array([1,2,3,4,5,6]).reshape(2,3)
+print(p)
+
+a.T
+np.squeeze(a)
+
+x=np.array([[[0],[1],[2]]])
+x.shape
+
+np.squeeze(x)
+
+np.squeeze(x).shape
