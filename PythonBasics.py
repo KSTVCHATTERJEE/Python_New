d7f9e549b09837005dccd55b8e142ee670a835bf
Kaustav Chatterjee <31096402+KSTVCHATTERJEE@users.noreply.github.com>
2018-02-12 12:06:18 +0530

    12th February 2018

--- PythonBasics.py
+++ PythonBasics.py
@@ -14,3 +14,21 @@ type(gender)
 names[0]
 'ashok'.upper()
 
+email_id = 'clin'
+if "@" not in email_id:
+    email_id += '@braindeis.edu'
+email_id  
+  
+line = "# This is a comment line \n"
+line
+line.strip()  
+line.rstrip()
+
+city_pop = {"NYC":8550405, "LA":456512, "CHI":3435435678}
+city_pop["NYC"]
+
+from scipy.stats.stats import pearsonr
+a=[1,4,6]
+b=[1,2,3]
+print(pearsonr(a,b))
+
