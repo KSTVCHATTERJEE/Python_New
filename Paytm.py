668c82fe19234cb327e8a36dab76cb00cb2a73e5
Kaustav Chatterjee <kstvchatterjee@gmail.com>
2018-04-04 22:06:01 +0530

    4th April 2018

---
+++ Paytm.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  4 21:18:03 2018
+
+@author: Kaustav Chatterjee
+"""
+
+from textblob import TextBlob
+import pandas as pd
+T = pd.read_csv('F:/Python_New/Manual_Sentiment.csv')
+print(T)
+type(T)
+FT=T['Formatted_Text']
+print(FT)
+type(FT)
+FT.ix[2]
+
+for i in range(299,401):
+    blobpol=TextBlob(FT.ix[i]).sentiment[0]
+    blobsub=TextBlob(FT.ix[i]).sentiment[1]
+    print(blob)
+    
+
